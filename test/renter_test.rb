98984d33b73448b74a065a5f4b1948fa36a710ab
James Belta <james.belta@gmail.com>
2020-03-31 09:10:01 -0600

    Added renter class, test for instance

---
+++ test/renter_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require "./lib/renter"
+
+class RenterTest < Minitest::Test
+
+  def test_it_exist
+    renter1 = Renter.new("Jessie")
+
+  assert_instance_of Renter, renter1
+
+end
