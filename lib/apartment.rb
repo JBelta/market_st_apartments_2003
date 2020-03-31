751e6b6e325a0e5b2c2f1337b10b1386b53ee1d8
James Belta <james.belta@gmail.com>
2020-03-31 10:34:10 -0600

    Can not shevel or push into @renter. @renter comes up as nil and not []. Push and << error message as undefined methods.

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,12 +1,18 @@
-class Apartment
-  attr_reader :renter, :number, :monthly_rent, :bathrooms, :bedrooms
+require "./lib/renter"
 
-  def initialize (number: nil, monthly_rent: nil, bathrooms: nil, bedrooms: nil)
-    @renter =[]
+class Apartment
+attr_reader :renter, :number, :monthly_rent, :bathrooms, :bedrooms
+
+  def initialize  (number: nil, monthly_rent: nil, bathrooms: nil, bedrooms: nil)
     @number = number
     @monthly_rent = monthly_rent
     @bathrooms = bathrooms
     @bedrooms = bedrooms
+    @renter = []
+  end
+
+  def add_renter(renter)
+      @renter << renter
   end
 
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -13,4 +13,15 @@ class ApartmentTest < Minitest::Test
     assert_equal 1, unit1.bathrooms
     assert_equal 1,unit1.bedrooms
   end
+
+  def test_at_add_renter
+    renter1 = Renter.new("Jessie")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal nil, @renter
+    require 'pry'; binding.pry
+    unit1.add_renter(renter1)
+    assert_equal renter1, @renter
+  end
+
 end
