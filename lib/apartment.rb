d4ea5e57a9d036376dcc037a742d2fef7545dada
James Belta <james.belta@gmail.com>
2020-03-31 09:37:52 -0600

    Added assert_equal A1, unit1.number. Pass

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,7 +1,7 @@
 class Apartment
   attr_reader :renter, :number, :monthly_rent, :bathrooms, :bedrooms
 
-  def initialize(number, monthly_rent, bathrooms, bedrooms)
+  def initialize (number: nil, monthly_rent: nil, bathrooms: nil, bedrooms: nil)
     @renter =[]
     @number = number
     @monthly_rent = monthly_rent
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,9 +4,10 @@ require './lib/renter'
 require './lib/apartment'
 
 class ApartmentTest < Minitest::Test
-  def it_exist
-    untit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  def test_it_exist
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
 
     assert_instance_of Apartment, unit1
+    assert_equal "A1", unit1.number
   end
 end
